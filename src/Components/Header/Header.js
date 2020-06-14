a50a1e60afcd386fe5932a96febd4f3eaa6db88b
Aman Arora <aman@stage3.co>
2020-06-14 21:55:58 +0530

    Added language option

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import 'antd/dist/antd.css';
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import './header.css'
 
-function Header() {
+function Header(props) {
+    const menu =(
+        <Menu>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" className="text-color-white" onClick={()=>props.changeLang('en')}>
+                    English
+                </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" className="text-color-white" onClick={()=>props.changeLang('de')}>
+                    Deutsche
+                </a>
+            </Menu.Item>
+        </Menu>
+    )
     return (
-        <div style={{background: '#1F2640', width: '100%'}}>
-            <div style={{display: 'flex', padding: '1% 12%'}}>
+        <div style={{background: '#1F2640', width: '100%', fontFamily: 'Roboto'}}>
+            <div className="header">
                 <img src={require('../../images/bs-logo-new.png')} alt="BlueStacks" title="BlueStacks" height = "50" style={{cursor: 'pointer'}} />
+                <button className="dropdown-button-style">
+                    <Dropdown overlay={menu}>
+                        <a className="ant-dropdown-link dropdown" style={{color: 'white'}} onClick={e => e.preventDefault()}>
+                            {props.lang === 'en'? 'English': 'Deutsche'}
+                         <DownOutlined />
+                        </a>
+                    </Dropdown>
+                </button>
             </div>
         </div>
     )
